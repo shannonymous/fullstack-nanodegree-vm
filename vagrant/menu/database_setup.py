ddbe65a47a105e8ae47c43104be0bd842fa11e60
Shannon Tjang <stjang@hubspot.com>
2019-03-04 17:30:10 +0800

    Updated import declarative_base

--- vagrant/menu/database_setup.py
+++ vagrant/menu/database_setup.py
@@ -1,11 +1,8 @@
 import sys
 
 from sqlalchemy import Column, ForeignKey, Integer, String
-
-from sqlalchemy.ext.declarative import from declarative_base
-
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
 from sqlalchemy import create_engine
 
 Base = declarative_base()
