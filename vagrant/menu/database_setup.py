32ca0d33ee2115448a343e99917209e027b8df2e
Shannon Tjang <stjang@hubspot.com>
2019-03-04 17:27:29 +0800

    Copied from https://github.com/udacity/Full-Stack-Foundations/blob/master/Lesson_1/database_setup.py (missing line 1 `import os`

--- vagrant/menu/database_setup.py
+++ vagrant/menu/database_setup.py
@@ -1,7 +1,6 @@
 import sys
 
-from sqlalchemy import
-Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 
 from sqlalchemy.ext.declarative import from declarative_base
 
@@ -13,14 +12,23 @@ Base = declarative_base()
 
 
 class Restaurant(Base):
-
-__tablename__ = 'restaurant'
+    __tablename__ = 'restaurant'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
 
 class menuItem(Base):
 
-__tablename__ = 'menu_item'
+    __tablename__ = 'menu_item'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
 
 ####end of file
 engine = create_engine('sqlite:///restaurantmenu.db')
 
-#Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
