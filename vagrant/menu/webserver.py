2f4afb33f71e36479592b9be2537e27cee66593c
Shannon Tjang <stjang@hubspot.com>
2019-03-10 20:03:03 +0800

    Edited href with correct single bracket

--- vagrant/menu/webserver.py
+++ vagrant/menu/webserver.py
@@ -20,7 +20,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>&#161;Hola!  <a href="https://www.w3schools.com">Visit W3Schools</a></body></html>"
+                output += "<html><body>&#161;Hola! <a href ='/hello'>Back to Hello</a></body></html>"
                 self.wfile.write(output)
                 print output
                 return
