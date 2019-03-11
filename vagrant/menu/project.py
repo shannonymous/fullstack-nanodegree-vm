f68984b5703ddcbe5489085ccd5e388921a27a72
Shannon Tjang <stjang@hubspot.com>
2019-03-11 14:30:27 +0800

    Updated Routing project from https://github.com/udacity/Full-Stack-Foundations/blob/master/Lesson-3/05_Routing-Create-Quiz-Template-Code/project.py

--- vagrant/menu/project.py
+++ vagrant/menu/project.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, menuItem
-app = Flask(__name__)
+from database_setup import Base, Restaurant, MenuItem
 
+app = Flask(__name__)
 
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
@@ -15,8 +15,8 @@ session = DBSession()
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
-    items = session.query(menuItem).filter_by(restaurant_id=restaurant.id)
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for i in items:
         output += i.name
@@ -27,6 +27,24 @@ def restaurantMenu(restaurant_id):
         output += '</br>'
         output += '</br>'
     return output
+
+# Task 1: Create route for newMenuItem function here
+
+
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
